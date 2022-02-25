1f879420a1519affbdf1c3efc32d65925cf5b1cc
rlw92 <r_l_williams@yahoo.com>
2022-02-25 17:21:34 +0000

    Made the grid, the s variable controls the sizing, needs to be accessed from the web page to work and need to implement colour changing script

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # etchasketchodin
 The etch a sketch project in the Odin Project
+
+The goal  is to make a square divs full of 16*16 other square divs that you can hove over and create a trail like an etch a sketch.
+Once made the next goal will be to add toggles to control different aspects such as the size of the divs and colour.
+
---
+++ js.js
@@ -0,0 +1,35 @@
+let main = document.querySelector(".main");
+
+
+
+//options for sizing: 16*16=256  32*32=1024   64*64=4096
+//padding =10px we have a border of 1px space=11px
+/*to align the boxes the main window width 
+needs to be 2*(11*sizingnumber) */
+//so for 16*16 width would be 2*(11*16), for 32 2*(11*32) etc
+/*we would then put another variable as the multipled return
+to keep the function below looping */
+/*we need to make this into an equation that works and can be 
+altered in the web page*/
+
+//let s be the amount you want, 7 for example would be a 7*7 grid//
+let s = 16;
+//mww can be main window width//
+let mww = 2*(11*s);
+// mw is gonna access main window of the grid
+let mw = document.querySelector(".main");
+mw.style.width = mww+"px";
+//loop can be the total amount of boxes needed so s*s or ://
+let loop = s**2;
+
+
+
+for(i=0;i<loop;i++){
+let div = document.createElement("div");
+div.className="grids";
+div.style.cssText ="border:solid 1px black;flex:auto;padding:10px";
+main.appendChild(div);
+
+
+}
+
